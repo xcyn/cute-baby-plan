275786479157ff4f0191f297aa20028a98c05fbc
xiongchuanyu <xiongchuanyu@zuoyebang.com>
2019-12-24 00:46:47 +0800

    fix: bug

--- servers/pm2.config.js
+++ servers/pm2.config.js
@@ -5,9 +5,8 @@ module.exports = {
     output: './logs/pm2/pm2.output.log',
     error_file: './logs/pm2/pm2.error.log',
     out_file: './logs//pm2pm2.combined.outerr.log',
-    merge_logs: true, // 集群的所有实例的日志文件合并
     log_date_format: 'YYYY-MM-DD HH:mm:ss.SSS',
-    instances: '1', //  进程数 1、数字 2、'max'根据cpu内核数
+    instances: 'max', //  进程数 1、数字 2、'max'根据cpu内核数
     watching: true,
     exec_mode: 'cluster',
     env_development: {
